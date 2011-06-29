f6e320dd739f5ab6ca45760f89ff39c3766d6904
Patrick DeVivo <patrick.devivo@gmail.com>
2011-06-29 17:24:55 -0400

    finished up the slider, yay\!

--- js/main.js
+++ js/main.js
@@ -1,51 +1,10 @@
 $(document).ready(function() {
-	$('#slider a:first').addClass('active');
-	
-	var numberImages = $('#slider a').length;
-	
-	
-	$('#slider a').each(function(index, value) {
-		$('#counter').append("<span id='bubble-" + index + "' class='bubble'></span>");
-		$(this).attr('id', 'slide-' + index + '')
-	})
-	
-	$('#slider .bubble').mouseover(function() {
-		$(this).addClass('active');
-	}).mouseout(function() {
-		$(this).removeClass('active');
-	})
-
-	
-	function slideLeft() {
-		
-	}
-	
-	function slideRight() {
-		var active = $('#slider a.active');
-		var next = active.next('a');
-		
-		if(next.length !== 0) {
-			active.animate({
-				left: '-580px'
-			}, function(){
-				$(this).removeClass('active').css('left', '0px');
-			});
-			
-			next.css('right', '-580px').addClass('active').animate({
-				right: '0px'
-			});
-		}
-		else {
-			//next = $('#slider a:first');
-			
-		}
-		
-
-		
-
-	}
-	
-	$('#button-right').click(function() {
-		slideRight();
+	var sudoSlider = $("#slide-contain").sudoSlider({
+        continuous: true,
+		history: true,
+		controlsFade: false,
+		prevHtml: '<span id="button-left"></span>',
+		nextHtml: '<span id="button-right"></span>',
+		numeric: true
 	});
 })
